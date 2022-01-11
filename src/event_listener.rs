3bc68610eb6e6d0ea621d102c80e23f40baedf63
tduong <duongt.sbc@gmail.com>
2022-01-10 19:40:10 -0800

    fix loading from non-existent file

--- src/event_listener.rs
+++ src/event_listener.rs
@@ -1,13 +1,15 @@
 use enigo::*;
 use std::fs::File;
 use std::io::Write;
+use std::path::Path;
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    //how to write tests better?
     #[test]
-    fn light_event_test() {
-        #[cfg(target_arch = "arm")] //welp, shitty test, but there's not much to test here ngl. It doesn't work on non-raspberry pis
+    #[should_panic(expected="[ERRO] Missing backlight control path /sys/class/backlight/rpi_backlight/brightness")] 
+    fn light_event_panic_test() {
         assert_eq!(light_event([0x50, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0]), 0x5);
     }
     #[test]
@@ -23,12 +25,15 @@ mod tests {
 pub fn light_event(data: [u8;8]) -> u8 {
     println!("[INFO] Recv: Headlight Event [{:x} {:x} {:x} {:x} {:x} {:x} {:x} {:x}]",
         data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7]);
+    if !Path::new("/sys/class/backlight/rpi_backlight/brightness").exists() {
+        panic!("[ERRO] Missing backlight control path /sys/class/backlight/rpi_backlight/brightness");
+    }
     let mut brightness_handle = File::open("/sys/class/backlight/rpi_backlight/brightness").unwrap();
     let mut _i: usize = 0;  //doing this quick hack to stop the match from bitching about the return type mismatch (because I'm lazy)
     let input = data[0] & 0xF0;
     match input {
-        0x50 | 0x40 => _i = brightness_handle.write(&[0x20]).unwrap(),  //headlights on
-        0x00        => _i = brightness_handle.write(&[0xff]).unwrap(),  //headlights off
+        0x50 | 0x40 => _i = brightness_handle.write(&[0x20]).unwrap(),  //headlights on, brightness down
+        0x00        => _i = brightness_handle.write(&[0xff]).unwrap(),  //headlights off, brightness up
         _           => println!("[WARN] Event not matched"),
     };
     return input;
@@ -38,7 +43,7 @@ pub fn steering_wheel_control_event(data: [u8;8]) -> u8 {
     println!("[INFO] Recv: Steering Wheel Control Event [{:x}]", data[1]);
     let mut _enigo = Enigo::new();
     let input = data[1] & 0x0F;
-    #[cfg(target_arch = "arm")]
+    #[cfg(not(test))]
     match input {
         0xA => _enigo.key_click(Key::Layout('P')),   //Source
         0xB => _enigo.key_click(Key::Layout('N')),   //Next
