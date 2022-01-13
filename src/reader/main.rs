49ae427e26ea5177da3b717d9a81e751f9967cf2
tduong <duongt.sbc@gmail.com>
2022-01-13 01:55:01 -0800

    fiddlyuck gtk

--- src/reader/main.rs
+++ src/reader/main.rs
@@ -5,20 +5,7 @@ use std::collections::HashMap;
 use std::mem;
 //use std::thread;
 
-use gtk;
-use glib;
-use gtk::prelude::*;
-
 #[path = "../lib/canframe.rs"] mod canframe;
-
-fn build_ui (app: &gtk::Application) {
-    let builder: gtk::Builder = gtk::Builder::from_file("window.glade");
-    let window: gtk::Window = builder.object("main_window").expect("unable to get main window from glade");
-    let entities: gtk::ListBox = gtk::ListBox::new();
-    
-    window.set_application(Some(app));
-    window.show_all();
-}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -27,7 +14,7 @@ fn main() {
         port_name = args[1].clone();
     }
 
-    let mut known_sensors = HashMap::new();
+    let mut known_sensors: HashMap<u32, u64> = HashMap::new();
 
     let ports = serialport::available_ports().unwrap();
     if port_name == "" {
@@ -43,37 +30,13 @@ fn main() {
     arduino_serial.set_timeout(Duration::new(1000000000,0))
         .expect("[ERRO] failed to set new timeout");
 
-    let app = gtk::Application::builder()
-        .application_id("com.canbusreader.CanbusReader")
-        .build();
-
-    app.connect_activate(|app| {
-        build_ui(&app);
-    });
-
-    glib::idle_add(move || {
-        //if there's nothing to read, continue or we'll be blocking the main thread
-        if arduino_serial.bytes_to_read().unwrap() <= 0 {
-            return Continue(true);
-        }
+    loop {
         let mut buf = [0;mem::size_of::<canframe::CanFrame>()];
         arduino_serial.read(&mut buf)
             .expect("[ERRO] Failed to get id");
         let frame = canframe::populate_canframe(&buf);
-        known_sensors.insert(frame.id, 64);
-        println!("{}", frame.id);
-        return Continue(true);
-    });
+        let data = known_sensors.entry(frame.id).or_insert(u64::from_be_bytes(frame.data));
+        println!("{:#010x} : {:#018x}", frame.id, data);
+    }
 
-    //thread::spawn(move || {
-    //    loop {
-    //        let mut buf = [0;mem::size_of::<canframe::CanFrame>()];
-    //        arduino_serial.read(&mut buf)
-    //            .expect("[ERRO] Failed to get id");
-    //        let frame = canframe::populate_canframe(&buf);
-    //        known_sensors.insert(frame.id, 64);
-    //    }
-    //});
-
-    app.run();
 }
