001c2a7fa01d5d0f994ac2e4280feb63090b7153
tduong <duongt.sbc@gmail.com>
2022-01-13 23:19:47 -0800

    add reader function to print values to terminal

--- src/reader/main.rs
+++ src/reader/main.rs
@@ -3,9 +3,13 @@ use std::env;
 use std::time::Duration;
 use std::collections::HashMap;
 use std::mem;
-//use std::thread;
 
 #[path = "../lib/canframe.rs"] mod canframe;
+
+struct SensorNumMap {
+    _data: u64,
+    index: usize,
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -14,7 +18,7 @@ fn main() {
         port_name = args[1].clone();
     }
 
-    let mut known_sensors: HashMap<u32, u64> = HashMap::new();
+    let mut known_sensors: HashMap<u32, SensorNumMap> = HashMap::new();
 
     let ports = serialport::available_ports().unwrap();
     if port_name == "" {
@@ -29,14 +33,30 @@ fn main() {
     // if serial data isn't constantly streaming, we time out.
     arduino_serial.set_timeout(Duration::new(1000000000,0))
         .expect("[ERRO] failed to set new timeout");
-
+    print!("\x1B[2J");
     loop {
         let mut buf = [0;mem::size_of::<canframe::CanFrame>()];
         arduino_serial.read(&mut buf)
             .expect("[ERRO] Failed to get id");
         let frame = canframe::populate_canframe(&buf);
-        let data = known_sensors.entry(frame.id).or_insert(u64::from_be_bytes(frame.data));
-        println!("{:#010x} : {:#018x}", frame.id, data);
-    }
 
+        let len: usize = known_sensors.len();
+        let mut _index: usize = 1;
+        match known_sensors.get(&frame.id) {
+            Some(x) => _index = x.index, //already has an index
+            None => {
+                _index = len;    //place new entry at end of list
+                known_sensors
+                    .insert(frame.id, 
+                        SensorNumMap{
+                            _data: u64::from_be_bytes(frame.data),
+                            index: len,
+                            });
+            },
+        }
+
+        
+        print!("\x1B[{};1H", _index+1);  //move cursor to row index+1 col 1
+        println!("{:#010x} : {:#018x} ({:3})", frame.id, u64::from_be_bytes(frame.data), _index);    //update entry in position
+    }
 }
